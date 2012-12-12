7d4caad0d9797d3d6ef1418756434c3a62ddf741
Ravi Mendis <rmendis@mac.com>
2012-12-12 17:55:39 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,12 @@
+This is a Lift version of the PopIdol app from Chapter 17 of "WebObjects Developer's Guide" by Ravi Mendis. i.e me :) 
+NOTE: one difference is the use of embedded SVG in HTML5 which can be handled by most modern browseers.
+
+1. Launch [SBT](http://code.google.com/p/simple-build-tool).
+
+        ./sbt
+
+2. Run Jetty
+
+        container:start
+
+3. Go to http://localhost:8080/.
---
+++ src/main/scala/com/svgobjects/snippet/Popidol.scala
@@ -0,0 +1,36 @@
+package com.svgobjects
+package snippet
+
+import net.liftweb.http.S
+import net.liftweb.http.SHtml
+import net.liftweb.util.Helpers.strToCssBindPromoter
+import net.liftweb.http.SessionVar
+import net.liftweb.util._
+import Helpers._
+import net.liftweb.common.Box
+import net.liftweb.common.Full
+import net.liftweb.common.Empty
+
+/**
+ * stateless
+ */
+object Popidol {
+  // app state
+  @volatile var gvotes:Double = 1
+  @volatile var wvotes:Double = 1
+  
+  def total = gvotes + wvotes  
+    
+  def render = {
+    S.param("vote") match {
+      case Full(vote) => vote match {
+      	case "will" => wvotes+=1 
+      	case "garath" => gvotes+=1
+      }
+      case Empty => {}
+    }
+    
+    "#garath [transform]" #> {"scale(" + gvotes/total + ")"} &
+  	"#will [transform]" #> {"scale(" + wvotes/total + ")"}
+  }
+}
